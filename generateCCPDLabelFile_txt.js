00a8aa2c5205bd5ce0362e46b8baa5d7ecbca5ed
Chizi <61510435+Chizi-P@users.noreply.github.com>
2021-06-29 13:45:13 +0800

    Update generateCCPDLabelFile_txt.js

--- generateCCPDLabelFile_txt.js
+++ generateCCPDLabelFile_txt.js
@@ -1,9 +1,13 @@
 const fs = require('fs')
 
-const CCPD = `${__dirname}/CCPD2019`
+const CCPD = 'CCPD2019'
+const CCPDPath = `${__dirname}/${CCPD}`
 
 // 定義為車牌的label
-const label = 0
+const label = ['license plate']
+const licensePlateLabel = label.indexOf('license plate')
+
+const imgSize = { w: 1160, h: 720, c: 3 }
 
 const provinces = ["皖", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏", "浙", "京", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂", "琼", "川", "贵", "云", "藏", "陕", "甘", "青", "宁", "新", "警", "学", "O"]
 const alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'O']
@@ -12,24 +16,25 @@ const ads = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P
 const splitFiles = ['blur', 'challenge', 'db', 'fn', 'rotate', 'tilt']
 
 // splitFiles.forEach(splitFile => {
-//     fs.readdir(`${CCPD}/ccpd_${splitFile}`, (err, files) => {
+//     fs.readdir(`${CCPDPath}/ccpd_${splitFile}`, (err, files) => {
 //         if (err) throw err
 //         files.forEach(file => {
 //             console.log(file)
 //             const fileName = file.replace(/.(jpg|jpeg|png)$/, '')
-//             fs.writeFile(`${CCPD}/ccpd_${splitFile}/${fileName}.txt`, txt(fileName), err => {
+//             const { boundingBoxCoordinates } = analyzeFileName(fileName)
+//             fs.writeFile(`${CCPDPath}/ccpd_${splitFile}/${fileName}.txt`, txt(boundingBoxCoordinates), err => {
 //                 if (err) throw err
 //                 console.log(`written ccpd_${splitFile}/${fileName}.txt`)
 //             })
 
-//             const licensePlateNumber = alpn(fileName)
+//             const licensePlateNumber = alpn(boundingBoxCoordinates)
 //             console.log(licensePlateNumber)
 //         })
 //     })
 // })
 
 splitFiles.forEach(splitFile => {
-    fs.readFile(`${CCPD}/splits/ccpd_${splitFile}.txt`, (err, data) => {
+    fs.readFile(`${CCPDPath}/splits/ccpd_${splitFile}.txt`, (err, data) => {
         if (err) throw err
         const paths = data.toString().split('\n')
         // 不要矩陣中最後一個空元素
@@ -37,7 +42,7 @@ splitFiles.forEach(splitFile => {
         paths.forEach(path => {
             path = path.replace(/.(jpg|jpeg|png)$/, '')
             const fileName = path.replace(`ccpd_${splitFile}/`, '')
-            fs.writeFile(`${CCPD}/${path}.txt`, txt(fileName), err => {
+            fs.writeFile(`${CCPDPath}/${path}.txt`, txt(fileName), err => {
                 if (err) throw err
                 console.log(`written ${path}.txt`)
             })
@@ -45,20 +50,70 @@ splitFiles.forEach(splitFile => {
     })
 })
 
-function txt(fileName) {
-        // let [area, tiltDegree, boundingBoxCoordinates, fourVerticesLocations, licensePlateNumber, brightness, blurriness] = fileName.split('-').map(e => e.split('_'))
-        const boundingBoxCoordinates = fileName.split('-')[2].split('_')
-
-        const [p1, p2] = boundingBoxCoordinates.map(p => p.split('&'))
-        const w = p2[0] - p1[0]
-        const h = p2[1] - p1[1]
-        const x = p1[0] + w / 2
-        const y = p1[1] + h / 2
-        return `${label} ${x} ${y} ${w} ${h}`
+function analyzeFileName(fileName) {
+    const [area, tiltDegree, boundingBoxCoordinates, fourVerticesLocations, licensePlateNumber, brightness, blurriness] = fileName.split('-').map(e => e.split('_'))
+    return { area, tiltDegree, boundingBoxCoordinates, fourVerticesLocations, licensePlateNumber, brightness, blurriness }
 }
 
 // Analyze the license plate number
-function alpn(fileName) {
-    const lpn = fileName.split('-')[4].split('_')
+function alpn(lpn) {
     return provinces[lpn[0]] + alphabets[lpn[1]] + lpn.slice(2).map(char => ads[char]).join('')
+}
+
+function analyzeBoundingBoxCoordinates(boundingBoxCoordinates) {
+    const [p1, p2] = boundingBoxCoordinates.map(p => p.split('&'))
+    return {
+        p1: { x: p1[0], y: p1[1] },
+        p2: { x: p2[0], y: p2[1] }
+    }
+}
+
+/*
+* txt格式
+* 類別 中心點x在圖片中的比例 中心點y在圖片中的比例 標注矩形寬佔圖片的比例 標注矩形高佔圖片的比例
+*/
+function txt(fileName) {
+    const { boundingBoxCoordinates } = analyzeFileName(fileName)
+    const { p1, p2 } = analyzeBoundingBoxCoordinates(boundingBoxCoordinates)
+    const w = (p2.x - p1.x) / imgSize.w
+    const h = (p2.y - p1.y) / imgSize.h
+    const x = (p1.x + w / 2) / imgSize.w
+    const y = (p1.y + h / 2) / imgSize.h
+    return `${licensePlateLabel} ${x} ${y} ${w} ${h}`
+}
+
+function xml(path) {
+    const [folder, file] = path.split('/')
+    const fileName = file.replace(/.(jpg|jpeg|png)$/, '')
+    const { boundingBoxCoordinates } = analyzeFileName(fileName)
+    const { p1, p2 } = analyzeBoundingBoxCoordinates(boundingBoxCoordinates)
+        
+    const xml = 
+        ```
+        <annotation>
+            <folder>${folder}</folder>
+            <filename>${file}</filename>
+            <size>
+                <width>${imgSize.w}</width>
+                <height>${imgSize.h}</height>
+                <depth>${imgSize.c}</depth>
+            </size>
+            <object>
+                <name>${'license plate'}</name>  <!--目标类别-->
+                <bndbox>  <!--GT矩形框坐标-->
+                    <xmin>${p1.x}</xmin>
+                    <xmax>${p2.x}</xmax>
+                    <ymin>${p1.y}</ymin>
+                    <ymax>${p2.y}</ymax>
+                </bndbox>
+                <truncated>0</truncated>  <!--物体是否被遮挡（>15%）-->
+                <difficult>0</difficult>  <!--物体是否难以辨别，主要指需结合背景才能判断出类别的物体-->
+            </object>
+            <segmented>0</segmented>  <!--是否有分割label-->
+        </annotation>
+        ```
+    fs.writeFile(`${folder}/${fileName}.xml`, err => {
+        if (err) throw err
+        console.log(`written ${folder}/${fileName}.xml`)
+    })
 }
